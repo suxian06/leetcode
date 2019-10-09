d31a17efb22a0e0414913afdabde27fc3c27aa77
suxian06 <suxian06@gmail.com>
2019-10-08 19:47:03 -0700

    class Solution(object):     def removeDuplicates(self, nums):

---
+++ 26.RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) < 2:
+            return len(nums)
+
+        prev = nums[0]
+        i = 1
+
+        while i != len(nums):
+
+            if prev == nums[i]:
+                nums.pop(i)
+            else:
+                prev = nums[i]
+                i += 1
+
+        return len(nums)
