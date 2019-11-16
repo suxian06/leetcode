5f7f7026e66b51125b0391e3f79d8441c37da845
suxian06 <suxian06@gmail.com>
2019-11-16 12:10:02 -0700

    Minimum Cost For Tickets

--- 983.MinimumCostForTickets.py
+++ 983.MinimumCostForTickets.py
@@ -1,13 +1,13 @@
-# Runtime: 44 ms, faster than 88.98% of Python3 online submissions for Minimum Cost For Tickets.
+# Runtime: 32 ms, faster than 99.79% of Python3 online submissions for Minimum Cost For Tickets.
 # Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Minimum Cost For Tickets.
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        calendar = list(range(days[-1] + 1))
-        travel_day = [True if x in days else False for x in calendar]
+
+        daysets = set(days)
         dp = [0 for i in range(days[-1] + 1)]
         c1, c2, c3 = 2**31, 2**31, 2**31
         for i in range(days[-1] + 1):
-            if travel_day[i]:
+            if i in daysets:
                 c1 = max(dp[i - 1],0) + costs[0]
                 if i > 7:
                     c2 = dp[i - 7] + costs[1]
@@ -20,5 +20,5 @@ class Solution:
                 dp[i] += min(c1,c2,c3)
             else:
                 dp[i] = dp[i - 1]
-        #print(dp)
+
         return dp[-1]
