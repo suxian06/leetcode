d256ab6b9e2f135686f1e2b50aa4c9a3087d7b70
suxian06 <suxian06@gmail.com>
2019-11-22 12:40:31 -0700

    Swap Nodes in Pairs

--- 24.SwapNodesinPairs.py
+++ 24.SwapNodesinPairs.py
@@ -26,3 +26,22 @@ class Solution:
             newList.val = head.val
 
         return newListHead
+
+# Runtime: 28 ms, faster than 94.87% of Python3 online submissions for Swap Nodes in Pairs.
+# Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions for Swap Nodes in Pairs.
+# Nice recursive solution learned from leetcode explore and discussion
+# https://leetcode.com/problems/swap-nodes-in-pairs/discuss/171788/Python-or-Dummynode
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head):
+        if not head or not head.next:
+            return head
+        second = head.next
+        head.next = self.swapPairs(second.next)
+        second.next = head
+        return second
