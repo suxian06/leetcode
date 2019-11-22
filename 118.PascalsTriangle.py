eb711d3ec7d3149bbf3051f26f07eacce70236f3
suxian06 <suxian06@gmail.com>
2019-11-22 13:24:31 -0700

    Pascals Triangle

---
+++ 118.PascalsTriangle.py
@@ -0,0 +1,13 @@
+# Runtime: 24 ms, faster than 64.72% of Python3 online submissions for Decode Ways.
+# Memory Usage: 12.7 MB, less than 16.00% of Python3 online submissions for Decode Ways.
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        T = [[1],[1,1]]
+        for i in range(2,numRows):
+            r = [1]*(i + 1)
+            for j in range(1,i):
+                r[j] = T[i - 1][j - 1] + T[i - 1][j]
+            T.append(r)
+
+        return T[:numRows]
