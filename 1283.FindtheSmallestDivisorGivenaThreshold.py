d472f8f7d1c33076f5915b11098bd6de81b3bf5e
suxian06 <suxian06@gmail.com>
2020-03-28 21:03:59 -0700

    Count Number of Nice Subarrays

---
+++ 1283.FindtheSmallestDivisorGivenaThreshold.py
@@ -0,0 +1,17 @@
+# Runtime: 404 ms, faster than 78.79% of Python3 online submissions for Find the Smallest Divisor Given a Threshold.
+# Memory Usage: 19.3 MB, less than 100.00% of Python3 online submissions for Find the Smallest Divisor Given a Threshold.
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        l, r = 1, max(nums)
+        while l <= r:
+            m = l + (r - l) // 2
+            cur = 0
+            for n in nums:
+                cur += n // m + 1 if n % m else n // m
+
+            if cur <= threshold:
+                r = m - 1
+
+            else:
+                l = m + 1
+        return l
